3c2ae80408d18b54e4499cfafae9c1592f156d6f
olegvalen <oleg.valen.com@gmail.com>
2018-05-16 21:52:39 +0300

    initial commit 16.05.2018

---
+++ controllers/CategoryController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oleg-d
+ * Date: 13.05.18
+ * Time: 17:16
+ */
+
+namespace app\controllers;
+
+use app\models\Category;
+use Yii;
+use yii\web\Controller;
+
+class CategoryController extends Controller
+{
+    public function actionIndex()
+    {
+
+        $data = [];
+
+        $data['filter'] = Category::getFilter(Yii::$app->request->get('category'));
+
+        return $this->render('index', $data);
+    }
+}
